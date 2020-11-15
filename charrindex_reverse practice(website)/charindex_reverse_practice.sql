310c248b3097fa49f11e28079759413b415c352f
zeki07k <67226258+zeki07k@users.noreply.github.com>
2020-11-15 13:08:11 +0300

    Update charindex_reverse_practice.sql

--- charrindex_reverse practice(website)/charindex_reverse_practice.sql
+++ charrindex_reverse practice(website)/charindex_reverse_practice.sql
@@ -10,9 +10,9 @@ with cte as ( select [WebsiteName],[WebsiteUrl],
 case
 when  charindex('.',REVERSE([WebsiteUrl])) = 0 
 then 'Invalid'
-when  left(reverse([WebsiteUrl]),charindex('.',reverse([WebsiteUrl]))) <> reverse([DomainName])
-then reverse(left(reverse([WebsiteUrl]),charindex('.',reverse([WebsiteUrl]))))
-else convert(varchar,charindex('.',REVERSE([WebsiteUrl])))
+when  left(reverse([WebsiteUrl]),charindex('.',reverse([WebsiteUrl]))) <> reverse([DomainName])  -- in order to find domain name such as .com, .au ,.mx.  
+then reverse(left(reverse([WebsiteUrl]),charindex('.',reverse([WebsiteUrl]))))                    -- www.google.com.mx is a wrong domain indication which is .com in domain column.
+else convert(varchar,charindex('.',REVERSE([WebsiteUrl])))                                            -- so it should be .mx for that reason we have to add this row as well .
 end as Domain
 ,[DomainName]
 from [dbo].[tblDomain] d 
